4c8555f17c5ad3aba10415e5a3376a5d08e2ce2a
Alexmiko <mr.coctail@mail.ru>
2020-11-23 13:53:12 +0300

    added posters whit info and fixed slider offers

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 @@include('swiper.min.js');
 
-const btnDown = document.querySelectorAll(`.card__btn-quantity-down`);
-const btnUp = document.querySelectorAll(`.card__btn-quantity-up`);
+const btnDown = document.querySelectorAll(`.card__btn-quantity-down`); // кнопка убавить количество товара в карточке
+const btnUp = document.querySelectorAll(`.card__btn-quantity-up`); // кнопка прибавить количество товара в карточке
 
+// клик по кнопке убавить количество товара
 btnDown.forEach(el => {
 	el.addEventListener('click', function () {
 		if (parseInt(el.nextElementSibling.innerText) > 1) {
@@ -11,6 +12,7 @@ btnDown.forEach(el => {
 	});
 });
 
+// клик по кнопке прибавить количество товара
 btnUp.forEach(el => {
 	el.onclick = function () {
 		el.previousElementSibling.innerText = parseInt(el.previousElementSibling.innerText) + 1;
@@ -18,11 +20,16 @@ btnUp.forEach(el => {
 });
 
 
+const moreProductsBtn = document.querySelector(`.btn--more-js`); //кнопка развернуть 
+const moreproductsBlock = document.querySelectorAll(`.tabs__block--more`); //скрыте блоки скарточками товаров
+
+// переключение по категориям каталога
 document.addEventListener('DOMContentLoaded', () => {
 	const tabs = document.querySelector(`.tabs`);
 	const tabsBlock = document.querySelectorAll(`.tabs__block`);
 	const tabsBtn = document.querySelectorAll(`.tabs__btn-js`);		
-
+	
+	// клик по табам в каталоге
 	if (tabs) {
 		tabs.addEventListener('click', (e) => {
 			if (e.target.classList.contains(`tabs__btn-js`)) {
@@ -35,18 +42,22 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		});
 	}
-
+// обработчик клика по иконкам категорий в каталоге
 	const tabsHandler = (path) => {
 		tabsBlock.forEach(el => {
 			el.classList.remove(`tabs__block--active`);
+		});
+		moreproductsBlock.forEach(el => {
+			el.classList.remove(`active`);
 		});
 		document.querySelector(`[data-tabs-target="${path}"]`).classList.add(`tabs__block--active`);
 	}
 });
 
-const moreProductsBtn = document.querySelector(`.btn--more-js`);
-const moreproductsBlock = document.querySelector(`.tabs__block--more`);
-
+// клик по кнопке развернуть
 moreProductsBtn.onclick = () => {
-	moreproductsBlock.classList.add(`active`);
+	moreproductsBlock.forEach(el => {
+		el.classList.add(`active`);
+	});
+	
 }
